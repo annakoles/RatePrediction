ffa513a05c8d1b01fbe557a0111744738668b363
annakoles <33730644+annakoles@users.noreply.github.com>
2022-10-04 10:55:19 +0300

    Add files via upload

--- src/main/java/ru/liga/RatesReading.java
+++ src/main/java/ru/liga/RatesReading.java
@@ -11,15 +11,15 @@ import java.util.List;
 import static ru.liga.App.PREDICT_DAYS;
 
 public class RatesReading {
+    public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     public static List<Rate> read7rates(String fileName) throws Exception {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         List<Rate> rates = new ArrayList<>();
 
         try (BufferedReader csvReader = new BufferedReader(new InputStreamReader(App.class.getResourceAsStream(fileName)))) {
             csvReader.readLine();
             for (int i = 0; i < PREDICT_DAYS; i++) {
                 String[] nextLine = csvReader.readLine().split(";");
-                rates.add(new Rate(Integer.parseInt(nextLine[0]), LocalDate.parse(nextLine[1], formatter), new BigDecimal(nextLine[2])));
+                rates.add(new Rate(Integer.parseInt(nextLine[0]), LocalDate.parse(nextLine[1], DATE_FORMATTER), new BigDecimal(nextLine[2])));
             }
         }
         return rates;
